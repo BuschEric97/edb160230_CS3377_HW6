af58601e6ca0eae4829f56215e8e49dc4c79a728
BuschEric97 <buscheric97@gmail.com>
2018-04-19 23:15:01 -0500

    Added header file and tried to implement binary file I/O (won't compile)

--- program6.cc
+++ program6.cc
@@ -6,25 +6,36 @@
 // edb160230@utdallas.edu
 
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include <cdk.h>
+#include <string>
+#include "program6.h"
 
 #define MATRIX_WIDTH 3
-#define MATRIX_HEIGHT 3
+#define MATRIX_HEIGHT 5
 #define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
+#define MATRIX_NAME_STRING "Binary File Contents"
 
 using namespace std;
 
 int main()
 {
-  // initialize variables
+  // open the binary file for later reading
+  ifstream binInfile("cs3377.bin", ios::in | ios::binary);
+  BinaryFileHeader myHeader;
+  string magicNumber = "Magic: ";
+  string versionNumber = "Version: ";
+  string numRecords = "NumRecords: ";
+  BinaryFileRecord myRecord[4];
+  
+  // initialize CDK variables
   WINDOW *window;
   CDKSCREEN *cdkScreen;
   CDKMATRIX *myMatrix;
   
   // set arrays for the displayed matrix
-  const char *rowTitles[MATRIX_HEIGHT + 1] = {"R0", "R1", "R2", "R3"};
+  const char *rowTitles[MATRIX_HEIGHT + 1] = {"R0", "R1", "R2", "R3", "R4", "R5"};
   const char *colTitles[MATRIX_WIDTH + 1] = {"C0", "C1", "C2", "C3"};
   int boxWidths[MATRIX_WIDTH + 1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int boxTypes[MATRIX_WIDTH + 1] = {vMIXED, vMIXED, vMIXED, vMIXED};
@@ -37,7 +48,7 @@ int main()
   initCDKColor();
   
   // create the matrix
-  myMatrix = newCDKMatrix(cdkScreen, CENTER, CENTER, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_NAME_STRING, (char **) colTitles, (char **) rowTitles, boxWidths, boxTypes, 1, 1, ' ', ROW, true, true, false);
+  myMatrix = newCDKMatrix(cdkScreen, CENTER, CENTER, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_NAME_STRING, (char **) rowTitles, (char **) colTitles, boxWidths, boxTypes, 1, 1, ' ', ROW, true, true, false);
   
   // check if the matrix was created properly
   if (myMatrix == NULL)
@@ -46,14 +57,23 @@ int main()
       exit(EXIT_FAILURE);
     }
   
+  // read in the header of the binary file
+  binInfile.read((char *) myHeader, sizeof(BinaryFileHeader));
+  magicNumber.append((string) myHeader.magicNumber);
+  versionNumber.append((string) myHeader.versionNumber);
+  numRecords.append((string) myHeader.numRecords);
+  
+  // fill in cells (1,1), (1,2), and (1,3) with the info from file header
+  setCDKMatrixCell(myMatrix, 1, 1, magicNumber.c_str());
+  setCDKMatrixCell(myMatrix, 1, 2, versionNumber.c_str());
+  setCDKMatrixCell(myMatrix, 1, 3, numRecords.c_str());
+  
   // draw the matrix
   drawCDKMatrix(myMatrix, true);
   
-  // fill in cell (2,2)
-  setCDKMatrixCell(myMatrix, 2, 2, "Oriponi");
-  drawCDKMatrix(myMatrix, true);
-  
-  sleep(5);
+  // wait for input
+  char dummy;
+  cin >> dummy;
   
   // cleanup the screen
   endCDK();
